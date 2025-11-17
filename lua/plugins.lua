e4c0856e5bd38f85c03ecee8f73708b07701cfae
Letder40 <adrian04rv@gmail.com>
2025-11-17 19:27:16 +0100

    fixed minor syntax err

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -59,6 +59,6 @@ return require('packer').startup(function(use)
     use {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
-        requires = { {"nvim-lua/plenary.nvim"} }
+        requires = {"nvim-lua/plenary.nvim"}
     }
 end)
