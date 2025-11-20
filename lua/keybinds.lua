5b322b2b8c079784f2f9c55f13a923fd8fa3f980
Letder40 <adrian04rv@gmail.com>
2025-11-20 04:05:55 +0100

    qf map

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -4,7 +4,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 -- Copy to clipboard
 vim.keymap.set("v", "<leader>y", "\"+y")
@@ -13,6 +12,10 @@ vim.keymap.set("n", "<leader>y", "\"+yy")
 -- Paste from clipboard
 vim.keymap.set({"n", "v"}, "<leader>p", "\"+p")
 vim.keymap.set({"n", "v"}, "<leader>P", "\"+P")
+
+-- quickfix
+vim.keymap.set({"n", "v"}, "<M-j>", function () vim.cmd('cnext') end)
+vim.keymap.set({"n", "v"}, "<M-k>", function () vim.cmd('cprev') end)
 
 -- telescope
 local builtin = require('telescope.builtin')
